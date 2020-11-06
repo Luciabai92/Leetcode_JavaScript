9b01aeb3e0be82e5664db6c1bbe1f36073618ef0
luciabai92 <bailu19920907@sina.com>
2020-11-06 09:50:38 -0800

    33_Binary_SearchII

--- Binary_Search/33_SearchinRotatedSortedArray.js
+++ Binary_Search/33_SearchinRotatedSortedArray.js
@@ -17,38 +17,40 @@ var search1 = function(nums, target) {
 var search = function(nums, target) {
   //find out the pivot
   let l = 0, r = nums.length-1;
-  while(l<r) {
+  while(l<=r) {
     let m = l + Math.floor((r-l)/2);
-    console.log("l",l,"r",r,"m",m);
+    console.log("before","l",l,"r",r,"m",m);
+    console.log(nums[m],nums[r]);
 
-    if(nums[m] <= nums[r]) {
-      r = m;
+    if(nums[m] <= nums[nums.length-1]) {
+      r = m-1;
     }else{
       l = m+1;
     }
+    console.log("after","l",l,"r",r,"m",m);
   }
   //rot = l: index of the smallest element
   let rot = l;
   console.log("rot",rot);
   //find out which side target lies
-  if(target >= nums[0]) {
-    l = 0;
-    r = rot;
-  }
   l = (target >= nums[0]) ? 0:rot;
   //target > nums[nums.length-1] in case [1,3]
-  r = (target >= nums[0]&&target > nums[nums.length-1]) ? rot:nums.length;
-
+  r = (target >= nums[0]&&target > nums[nums.length-1]) ? rot:nums.length-1;
+  console.log("l",l,"r",r);
   //search target
-  while(l<r) {
+  while(l<=r) {
     let m = l + Math.floor((r-l)/2);
+    console.log("l",l,"r",r,"m",m);
+
+    if(nums[m] === target) return m;
+
     if(nums[m] >= target) {
-      r = m;
+      r = m-1;
     }else{
       l = m+1;
     }
   }
-  return (nums[l]===target) ? l:-1;
+  return -1;
 }
 
-console.log(search([5,1],1))
+console.log(search([8,9,2,3,4],2))
