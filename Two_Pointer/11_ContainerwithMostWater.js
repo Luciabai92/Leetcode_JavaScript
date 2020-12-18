41ba1feb85dd5778957c34d80191e9edfd9be67e
luciabai92 <bailu19920907@sina.com>
2020-12-17 16:02:11 -0800

    11

---
+++ Two_Pointer/11_ContainerwithMostWater.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+  let left = 0;
+  let right = height.length-1
+  let maxarea = 0;
+
+  while(left<right) {
+    maxarea = Math.max(maxarea, Math.min(height[left],height[right])*(right-left));
+    if(height(left) <= height(right)) {
+      left++
+    }else {
+      right++
+    }
+  }
+  return maxarea;
+};
